996a69fd462068424c4ceeea26c2c9839c17b235
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-10-07 21:27:48 +0300

    a

--- src/js/api.js
+++ src/js/api.js
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  '39899742-de5fd769b68a5bc4e589c2cf0';
+axios.defaults.baseURL = 'https://pixabay.com';
+
+export async function getImages(value) {
+  try {
+    const resp = await axios.get('/api', {
+      params: {
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    console.log(resp);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
+import { formRef } from './refs';
+import { getImages } from './api';
+
+formRef.addEventListener('submit', onSubmitHandler);
+
+function onSubmitHandler(event) {
+  event.preventDefault();
+
+  const input = event.currentTarget.elements.searchQuery;
+  const inputValue = input.value.trim();
+
+  getImages(inputValue);
+
+  event.currentTarget.reset();
+}
